2c0f48273fafa1babcbc68399ede0832815d47b7
Tom Mertens <your.email@whatev.er>
2021-05-25 12:25:56 +0200

    removed template tests

--- pyelegantsdds/__init__.py
+++ pyelegantsdds/__init__.py
@@ -8,15 +8,3 @@ Top-level package for pyelegantsdds.
 """
 
 __version__ = "0.1.0"
-
-
-def hello(who='world'):
-    """'Hello world' method.
-
-    :param str who: whom to say hello to
-    :returns: a string
-    """
-    result = "Hello " + who
-    return result
-
-# Your code here...
